ab48919fc13ca557bec2b70f9d79753481a2c499
Nathan Orsmond <orsmondn@hotmail.com>
2020-10-15 16:02:33 +0100

    Update to pom.xml, created TicketApplication.java for Spring boot build and updates to application.properties

---
+++ src/main/java/com/qa/sfia3/TicketApplication.java
@@ -0,0 +1,13 @@
+package com.qa.sfia3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@SpringBootApplication
+public class TicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketApplication.class, args);
+    }
+}
