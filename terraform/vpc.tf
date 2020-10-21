bdcbf66669bcc7b587928cb98cccb3736fd5bead
Jake Stone <jakestone4327@gmail.com>
2020-10-21 19:34:42 +0100

    VPC and cluster working, apply works - public DNS is not attaching to EC2 instances, will attach an elastic IP to public subnet

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,7 @@
 # -------------------- VPC ---------------------------
 resource "aws_vpc" "main" {
-  cidr_block       = "${var.vpc_cidr}"
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
   tags = {
     Name = "SFIA3"
     Team = "4"
@@ -8,27 +9,37 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_internet_gateway" "terra_igw" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
   tags = {
     Name = "main"
   }
 }
 
 resource "aws_subnet" "public" {
-  count = "${length(var.subnets_cidr)}"
-  vpc_id = "${aws_vpc.main.id}"
-  cidr_block = "${element(var.subnets_cidr,count.index)}"
-  availability_zone = "${element(var.azs,count.index)}"
+  count = length(var.subnets_cidr_public)
+  vpc_id = aws_vpc.main.id
+  cidr_block = element(var.subnets_cidr_public,count.index)
+  availability_zone = element(var.azs-public,count.index)
   tags = {
-    Name = "Subnet-${count.index+1}"
+    Name = "Public-${count.index+1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.subnets_cidr_private)
+  vpc_id = aws_vpc.main.id
+  cidr_block = element(var.subnets_cidr_private,count.index)
+  availability_zone = element(var.azs-private,count.index)
+  tags = {
+    Name = "Private-${count.index+1}"
   }
 }
 
 resource "aws_route_table" "public_rt" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.terra_igw.id}"
+    gateway_id = aws_internet_gateway.terra_igw.id
   }
   tags = {
     Name = "publicRouteTable"
@@ -36,9 +47,8 @@ resource "aws_route_table" "public_rt" {
 }
 
 resource "aws_route_table_association" "a" {
-  count = "${length(var.subnets_cidr)}"
-  subnet_id      = "${element(aws_subnet.public.*.id,count.index)}"
-  route_table_id = "${aws_route_table.public_rt.id}"
+  subnet_id      = aws_subnet.public[0].id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 # ------------------- ^VPC^ --------------------------
