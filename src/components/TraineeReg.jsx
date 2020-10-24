9c2e36572826ea5266de8dec55f419927e56f062
Nathan Orsmond <orsmondn@hotmail.com>
2020-10-24 17:30:05 +0100

    added form for trainee registration

--- src/components/TraineeReg.jsx
+++ src/components/TraineeReg.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const TraineeReg = () => {
     return (
         <Jumbotron>
-
+            <h1>Who are you?</h1>
+            <Form>
+                <Form.Group controlId="formForename">
+                    <Form.Label>Forename</Form.Label>
+                    <Form.Control type="text"></Form.Control>
+                </Form.Group>
+                <Form.Group controlId="formSurname">
+                    <Form.Label>Surname</Form.Label>
+                    <Form.Control type="text"></Form.Control>
+                </Form.Group>
+                <Form.Group controlId="formCohort">
+                    <Form.Label>Cohort</Form.Label>
+                    <Form.Control as="select">
+                        <option>placeholder1</option>
+                        <option>placeholder2</option>
+                        <option>placeholder3</option>
+                    </Form.Control>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </Jumbotron>
     );
 }
