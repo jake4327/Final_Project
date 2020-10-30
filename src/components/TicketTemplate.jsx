59f161a0c5f1a1b4f77c007a4102cbcfae482e7f
Nathan Orsmond <orsmondn@hotmail.com>
2020-10-30 10:14:40 +0000

    added delete functionality to tickettemplate component, included modal which gives user confirmation on the ticket they want to delete to ensure they don't misclick

--- src/components/TicketTemplate.jsx
+++ src/components/TicketTemplate.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useHistory} from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import axios from 'axios';
 
 const TicketTemplate = (props) => {
     const history = useHistory();
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const handleStatus = (props) => {
         let status = props.status;
@@ -23,28 +29,49 @@ const TicketTemplate = (props) => {
             state:{
                 data:data
             }
-        });    
+        }); 
+    }
+
+    const deleteTicket = () => {
+        let data = props.data;
+        console.log(data);
+        axios.delete(`http://localhost:8080/deleteTicket/${data.ticketId}`)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+        history.go(0);
     }
 
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                <Card.Title>Title: {props.data.title}</Card.Title>
-                <Card.Text>
-                    Description: {props.data.description}
-                </Card.Text>
-                <Card.Text>
-                    Topic: {props.data.topic}
-                </Card.Text>
-                <Card.Text>
-                    Solved: {handleStatus(props.data.status)}
-                </Card.Text>
-            </Card.Body>
-            <Card.Body>
-                <Button variant="primary" onClick={handleEditClick}>Edit</Button>
-                <Button variant="primary">Delete</Button>
-            </Card.Body>
-        </Card>
+        <>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Title: {props.data.title}</Card.Title>
+                    <Card.Text>
+                        Description: {props.data.description}
+                    </Card.Text>
+                    <Card.Text>
+                        Topic: {props.data.topic}
+                    </Card.Text>
+                    <Card.Text>
+                        Solved: {handleStatus(props.data.status)}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Body>
+                    <Button variant="primary" onClick={handleEditClick}>Edit</Button>
+                    <Button variant="primary" onClick={handleShow}>Delete</Button>
+                </Card.Body>
+            </Card>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>Are you sure you want to delete this ticket?</Modal.Header>
+                <Modal.Body>
+                    <Button variant="primary" onClick={deleteTicket}>Delete</Button>
+                </Modal.Body>
+            </Modal>
+        </>
     );
 }
 
