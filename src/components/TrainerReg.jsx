7fb6897017aaf087e49517e0adf7173ce8c80bb6
Nathan Orsmond <orsmondn@hotmail.com>
2020-11-02 17:10:21 +0000

    added dependencies to package-lock.json file

--- src/components/TrainerReg.jsx
+++ src/components/TrainerReg.jsx
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 const TrainerReg = () => {
+    const [trainers, setTrainers] = useState([]);
+    const [error, setError] = useState(null);
+
     const handleClick = (click) => {
         click.preventDefault();
         window.location.href="/home";
     }
-    return (
-        <Jumbotron>
-            <Button variant="primary" type="submit" size="lg" onClick={handleClick}>
-                Placeholder 1
-            </Button>
-            <Button variant="primary" type="submit" size="lg" onClick={handleClick}>
-                Placeholder 2
-            </Button>
-            <Button variant="primary" type="submit" size="lg" onClick={handleClick}>
-                Placeholder 3
-            </Button>
-        </Jumbotron>
-    );
+    
+    useEffect( () => {
+        axios.get("http://localhost:8080/getAllTrainers")
+        .then(res => res)
+        .then(
+            (res) => {
+                setTrainers(res.data);
+            },
+            (error) => {
+                setError(error);
+            }
+        )
+    },[]);
+    if (error) {
+        return <div>Oops... Something went wrong... {error.message}</div>
+    } else {
+        return (
+            <Jumbotron>
+                {trainers.map( trainer => (
+                    <div>
+                        #######
+                    <Button variant="primary" type="submit" onClick={handleClick}>
+                        {trainer.forename} {trainer.surname} Testing
+                    </Button>
+                    </div>
+                ))}
+            </Jumbotron>
+        );
+    }
+
 }
 
 export default TrainerReg;
