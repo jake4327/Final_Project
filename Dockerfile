8c1f6f7b41717c0d272c66065b1fac38de50a0e6
Nathan Orsmond <59610591+Nathan2820@users.noreply.github.com>
2020-11-09 15:50:42 +0000

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-# pulling official image of Java
-#FROM openjdk:8-jdk-alpine
-#VOLUME /tmp
-#ADD target/sfia3.jar app.jar
-#ENTRYPOINT ["java","-jar","/app.jar"]
-
-# pulling offical image of Nexus 
-FROM sonatype/nexus3
-LABEL maintainer="rpscdevelopments@gmail.com"
-# copy configuration file to docker container
-COPY /Nexus/nexus.properties nexus-data/etc/nexus.properties
+FROM node
+# set working directory
+WORKDIR /app
+# install app dependencies
+COPY package.json ./
+COPY package-lock.json ./
+# add app
+COPY . ./
+RUN npm install
+EXPOSE 3000
+# start app
+CMD ["npm", "start"]
---
+++ src/components/CreateTicket.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
+
+const CreateTicket = () => {
+    const [title, setTitle] = useState([]);
+    const [description, setDescription] = useState([]);
+    const [topic, setTopic] = useState([]);
+    const history = useHistory();
+    let data = JSON.parse(localStorage.getItem("user"));
+
+    const handleClick = (click) => {
+        click.preventDefault();
+        axios.post(`http://${process.env.REACT_APP_BACKEND_API}:8080/addTicket`, {
+            title: title,
+            description: description,
+            topic: topic,
+            trainee: {
+                traineeId: data.traineeId
+            }
+        })
+        .then(function (response) {
+            console.log(response);
+            history.push("/home");
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    const handleTitle = event => {
+        setTitle(event.target.value);
+    }
+
+    const handleDescription = event => {
+        setDescription(event.target.value);
+    }
+
+    const handleTopic = event => {
+        setTopic(event.target.value);
+    }
+
+    return (
+        <>
+            <Jumbotron>
+                <Navbar/><br/>
+                <Form>
+                    <Form.Group controlId="formTitle">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Title" onChange={handleTitle}></Form.Control>
+                        <Form.Text>
+                            Concise identifier for the problem
+                        </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="formDescription">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control as="textarea" rows={5} placeholder="Enter Description" onChange={handleDescription}></Form.Control>
+                        <Form.Text>
+                            An explanation of what the issue is and all relevant information that can assist in solving the problem
+                        </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="formTopic">
+                        <Form.Label>Topic</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Topic" onChange={handleTopic}></Form.Control>
+                        <Form.Text>
+                            What type of problem is it? (Examples: React, Angular, Spring Boot, Jenkins, Terraform, CLI)
+                        </Form.Text>
+                    </Form.Group>
+                    <Button variant="primary" type="submit" onClick={handleClick}>
+                        Submit
+                    </Button>
+                </Form>
+            </Jumbotron>
+        </>
+    );
+}
+
+export default CreateTicket;
